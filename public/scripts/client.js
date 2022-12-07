c9cd1bfdbf3018369c31ff31ad59a1bdba4ccc34
nawalabdi <nawalabdi03@gmail.com>
2022-12-07 17:26:20 -0500

    render tweets using ajax, create function that uses JQUERY to make request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,57 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-
-
-
-
-
-
-
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
-
-
-
 $(document).ready(function () {
   const createTweetElement = function (tweetData) {
     const $tweet = $(`
@@ -84,21 +33,44 @@ $(document).ready(function () {
 
   }
   const renderTweets = function (tweets) {
+    $("#tweet-container").empty();
     for (const item of tweets) {
       const $tweet = createTweetElement(item);
-      $(`#tweet-container`).append($tweet)
+      $(`#tweet-container`).prepend($tweet)
     }
   }
+  
+
+  const loadTweets = function() {
+    $.get("/tweets", function(data){
+      renderTweets(data)
+      console.log(data);
+    });
+    
+  }
+  loadTweets()
 
 
-  renderTweets(data);
+  $("#my-form").on("submit", function (event) {
+    event.preventDefault();
+    const formData = ($(this).serialize());
+    console.log("HELLO FROM JQUERY")
+    
+    $.ajax({
+      type: "POST",
+      url: '/tweets/',
+      data:formData,
+      success:() => {loadTweets()},
+    })
 
+  
+
+    
+  })
+  
 })
 
 
 
-// // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('#tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
 
